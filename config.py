d42f020a803cdcabb82844a4b71295fe476b2b56
UtilityAnon1 <utilityanon@gmail.com>
2025-02-07 18:37:13 +0000

    Auto-backup: 2025-02-07 18:37:13

--- config.py
+++ config.py
@@ -11,7 +11,8 @@ DUTY_MODE_ACTIVE = True
 OWNER_TELEGRAM_ID = "YOUR_TELEGRAM_USER_ID"  # Replace with your Telegram user ID
 
 # Git Configuration
-GIT_REPO_URL = "https://github.com/UtilityAnon1/Telegrambot.git"
+GITHUB_TOKEN = os.environ.get('GITHUB_TOKEN')
+GIT_REPO_URL = f"https://{GITHUB_TOKEN}@github.com/UtilityAnon1/Telegrambot.git" if GITHUB_TOKEN else "https://github.com/UtilityAnon1/Telegrambot.git"
 GIT_BRANCH = "main"
 GIT_USERNAME = "UtilityAnon1"
 GIT_EMAIL = "utilityanon@gmail.com"
